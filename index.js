ca57b55c5b303841bb6aede8ceaf835eb9987ab0
Anset App <isultanfx@gmail.com>
2019-09-08 18:24:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,9 @@ const ipLocation = require('iplocation').default
 const emoji_source = 'https://unpkg.com/country-flag-emoji-json@1.0.2/json/flag-emojis-by-code.pretty.json'
 const axios = require('axios')
 
-exports.getEmoji = (ipAddress) => {
-    var emoji = ''
+exports.getEmoji = async (ipAddress) => {
 
-    ipLocation(ipAddress)
-    .then((res) => {
-        var countryCode = res.countryCode
-        axios.get(emoji_source).then(function (response) {
-            emoji = response.data[countryCode].emoji
-        })
-        .catch(function (error) {
-            console.error(error)
-        })
-    })
-    .catch(error => {
-        console.error(error)
-    });
-return emoji
+    let locationInfo = await ipLocation(ipAddress)
+    let emoji_dic = await axios.get(emoji_source)
+    return emoji_dic.data[locationInfo.countryCode].emoji
 }
